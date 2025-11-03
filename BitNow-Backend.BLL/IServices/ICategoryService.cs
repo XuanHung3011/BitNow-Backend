00d1ccd13a72b26360d23bbdd94df63c1f476ae0
hungnd29523 <hungndhe172631@fpt.edu.vn>
2025-11-03 22:00:01 +0700

    fix confix CategoryDTOs

--- BitNow-Backend.BLL/IServices/ICategoryService.cs
+++ BitNow-Backend.BLL/IServices/ICategoryService.cs
@@ -4,12 +4,12 @@ namespace BitNow_Backend.BLL.IServices
 {
     public interface ICategoryService
     {
-        Task<IEnumerable<CategoryDto>> GetAllCategoriesAsync();
-        Task<PaginatedResult<CategoryDto>> GetCategoriesPagedAsync(CategoryFilterDto filter);
-        Task<CategoryDto?> GetCategoryByIdAsync(int id);
-        Task<CategoryDto?> GetCategoryBySlugAsync(string slug);
-        Task<CategoryDto> CreateCategoryAsync(CreateCategoryDto createCategoryDto);
-        Task<CategoryDto?> UpdateCategoryAsync(int id, UpdateCategoryDto updateCategoryDto);
+        Task<IEnumerable<CategoryDtos>> GetAllCategoriesAsync();
+        Task<PaginatedResult<CategoryDtos>> GetCategoriesPagedAsync(CategoryFilterDto filter);
+        Task<CategoryDtos?> GetCategoryByIdAsync(int id);
+        Task<CategoryDtos?> GetCategoryBySlugAsync(string slug);
+        Task<CategoryDtos> CreateCategoryAsync(CreateCategoryDtos createCategoryDtos);
+        Task<CategoryDtos?> UpdateCategoryAsync(int id, UpdateCategoryDtos updateCategoryDtos);
         Task<bool> DeleteCategoryAsync(int id);
         Task<bool> CategoryExistsAsync(int id);
         Task<bool> SlugExistsAsync(string slug, int? excludeId = null);
