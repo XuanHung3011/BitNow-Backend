d3ab1b641f7740261a1be0ed076c1a51d98bab0c
XuanHung <hungtxhe171405@fpt.edu.vn>
2025-11-19 02:15:39 +0700

    Update IBidService.cs

--- BitNow-Backend.BLL/IServices/IBidService.cs
+++ BitNow-Backend.BLL/IServices/IBidService.cs
@@ -4,11 +4,12 @@ namespace BitNow_Backend.BLL.IServices
 {
 	public interface IBidService
 	{
-		Task<BidResultDto> PlaceBidAsync(int auctionId, int bidderId, decimal amount);
+		Task<BidResultDto> PlaceBidAsync(int auctionId, int bidderId, decimal amount, bool isAutoBid = false);
 		Task<IReadOnlyList<BidDto>> GetRecentBidsAsync(int auctionId, int limit);
 		Task<decimal?> GetHighestBidAsync(int auctionId);
         Task<PaginatedResultB<BiddingHistoryDto>> GetBiddingHistoryAsync(int bidderId, int page, int pageSize);
     }
 }
+
 
 
